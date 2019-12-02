7576311e64d4a1ed376e32d9447caa72b4c38280
student <student@win2012base>
2019-12-02 01:03:18 -0800

    commiting

--- customer-service/src/main/java/com/casestudy/customer/RabbitMQConfig.java
+++ customer-service/src/main/java/com/casestudy/customer/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.casestudy.customer;
 
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -9,21 +8,20 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-	@Value("${customer.rabbitmq.queue}")
-	String queueName;
+	//@Value("${customer.rabbitmq.queue}")
+	String queueName = "customer.rabbitmq.queue";
 
-	@Value("${customer.rabbitmq.exchange}")
-	String exchange;
+	//@Value("${customer.rabbitmq.exchange}")
+	String exchange = "customer.rabbitmq.exchange";
 
-	@Value("${customer.rabbitmq.routingkey}")
-	private String routingkey;
+	//@Value("${customer.rabbitmq.routingkey}")
+	private String routingkey="customer.rabbitmq.routingkey";
 
 	@Bean
 	Queue queue() {
@@ -44,10 +42,9 @@ public class RabbitMQConfig {
 	public MessageConverter jsonMessageConverter() {
 		return new Jackson2JsonMessageConverter();
 	}
-
 	
 	@Bean
-	public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
 		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 		rabbitTemplate.setMessageConverter(jsonMessageConverter());
 		return rabbitTemplate;

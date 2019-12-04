35b41d5c8274e63aa9349e4ccdd4e91cb44f1aeb
student <student@win2012base>
2019-12-04 03:59:54 -0800

    added

--- sales-order-service/src/main/java/com/casestudy/salesorder/ItemService.java
+++ sales-order-service/src/main/java/com/casestudy/salesorder/ItemService.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="item-service", fallback = ItemServiceFallback.class)
-@RibbonClient(name="item-service")
+@FeignClient(name="item-service-491968", fallback = ItemServiceFallback.class)
+@RibbonClient(name="item-service-491968")
 public interface ItemService {
 	
 	@GetMapping("/item/{itemName}")
--- sales-order-service/src/main/java/com/casestudy/salesorder/SalesOrderConfig.java
+++ sales-order-service/src/main/java/com/casestudy/salesorder/SalesOrderConfig.java
@@ -3,7 +3,7 @@ package com.casestudy.salesorder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix="sales-order-service")
+@ConfigurationProperties(prefix="sales-order-service-491968")
 @Component
 public class SalesOrderConfig {
 
